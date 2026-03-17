01ef4b75214b9ebb66db1ec3f85cb31be408419e
jrrubimar7 <raysinternational999@gmail.com>
2026-03-17 22:59:04 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='aura-v10';
-self.addEventListener('install',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim().then(()=>self.clients.matchAll({type:'window'}).then(cs=>cs.forEach(c=>c.navigate(c.url)))));});
-self.addEventListener('fetch',e=>{});
